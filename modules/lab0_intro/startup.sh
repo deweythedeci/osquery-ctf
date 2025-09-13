763f42ec636a5d3840042926b0005725d72f3f81
deweythedeci <ben.c.chong@gmail.com>
2025-09-13 13:33:09 -0700

    added fix to install add-apt-repository in startup script

--- modules/lab0_intro/startup.sh
+++ modules/lab0_intro/startup.sh
@@ -1,4 +1,5 @@
 sudo mkdir -p /etc/apt/keyrings
 curl -L https://pkg.osquery.io/deb/pubkey.gpg | sudo tee /etc/apt/keyrings/osquery.asc
+sudo apt install software-properties-common -y
 sudo add-apt-repository 'deb [arch=amd64 signed-by=/etc/apt/keyrings/osquery.asc] https://pkg.osquery.io/deb deb main'
 sudo apt install osquery -y
