2b9e78675fe7f094f5bd3accfe61c8d73a441552
deweythedeci <ben.c.chong@gmail.com>
2025-09-13 13:21:55 -0700

    update

--- modules/lab0_intro/main.tf
+++ modules/lab0_intro/main.tf
@@ -5,7 +5,7 @@ resource "google_compute_instance" "lab0_instance" {
   
   boot_disk {
     initialize_params {
-      image = "ubuntu-os-cloud/ubuntu-2204-lts"
+      image = "debian-cloud/debian-13"
     }
   }
 
