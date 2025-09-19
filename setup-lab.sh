d6cb0983310de10e191725d74de668e303c00b8b
chongben <chongben@pdx.edu>
2025-09-19 12:18:42 -0700

    added comments to setup

--- setup-lab.sh
+++ setup-lab.sh
@@ -1,4 +1,11 @@
+# Get current project id
 PROJECT_ID=$(gcloud config get-value project)
+
+# Put project id into terraform.tfvars so terraform can function
 echo "project_id = \"${PROJECT_ID}\"" > terraform/core/terraform.tfvars
+
+# Generate ssh key pair for ansible to access VMs
 ssh-keygen -t ed25519 -f ssh/id_ed25519 -P ""
+
+# Initialize terraform
 terraform -chdir=terraform/core init
