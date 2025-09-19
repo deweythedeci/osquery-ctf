005f6f318d7759a926c4c2e31618b6e1a38980d7
chongben <chongben@pdx.edu>
2025-09-19 12:16:50 -0700

    fixed setup

--- setup-lab.sh
+++ setup-lab.sh
@@ -1,4 +1,4 @@
 PROJECT_ID=$(gcloud config get-value project)
-echo 'project_id = "${PROJECT_ID}"' > terraform/core/terraform.tfvars
+echo "project_id = \"${PROJECT_ID}\"" > terraform/core/terraform.tfvars
 ssh-keygen -t ed25519 -f ssh/id_ed25519 -P ""
 terraform -chdir=terraform/core init
