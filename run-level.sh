9df01d7483f9e01367353dd2b79e39d1e03be881
deweythedeci <ben.c.chong@gmail.com>
2025-09-18 18:46:18 -0700

    update

--- run-level.sh
+++ run-level.sh
@@ -10,7 +10,10 @@ if [ -z "$LEVEL" ]; then
 fi
 
 echo "Deploying module: $LEVEL..."
-terraform -chdir=terraform apply -target="module.${LEVEL}" -auto-approve
-terraform -chdir=terraform output -raw "${LEVEL}_ip"
+terraform -chdir=terraform apply -target="module.${LEVEL}" -auto-approve > /dev/null
+
+IP = $(terraform -chdir=terraform output -raw "${LEVEL}_ip")
+
+ansible-playbook -i "${IP}," -u "student" --private-key ssh/id_ed25519 "ansible/${LEVEL}/playbook.yml"
 
 echo "Level Deployed!"
