075fe6ae70572dcef748ea377dbc85ddef539b18
chongben <chongben@pdx.edu>
2025-10-03 13:56:50 -0700

    fixed startup script

--- terraform/lab0_intro/startup.sh
+++ terraform/lab0_intro/startup.sh
@@ -1,4 +1,5 @@
+sudo apt install gpg -y
 curl -fsSL  https://pkg.osquery.io/deb/pubkey.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/osquery.gpg
 echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/osquery.gpg] https://pkg.osquery.io/deb deb main" | sudo tee /etc/apt/sources.list.d/osquery.list > /dev/null
 sudo apt update
-sudo apt install osquery
+sudo apt install osquery -y
